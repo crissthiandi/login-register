f97b92aa40687eee12432ecddd0077535f2289f5
crissthiandi <albertocenaa@gmail.com>
2021-02-11 00:44:35 -0600

    Corregimos nombre de vista login por Login Se importan las 3 vistas al index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import SingUp from "@/views/SingUp.vue";
+import Private from "@/views/Private.vue";
 
 Vue.use(VueRouter);
 
