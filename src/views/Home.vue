c3535acabed88c3acd227f03e16146569d566683
crissthiandi <albertocenaa@gmail.com>
2021-02-17 11:25:36 -0600

    Se agrega las clases para darle una mejor vista a los div usando tailwind, Fix #2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
+  <div class="flex justify-center items-center">
     <h1>
       Esta es una pagina publica
     </h1>
-    <router-link to="/login">Login</router-link>
-    <router-link to="/Sign-Up">Sign Up</router-link>
-
+    <router-link class="m-8 p-3 px-4 font-bold text-white rounded-lg bg-green-400" to="/login">Login</router-link>
+    <router-link class="m-8 p-3 px-4 font-bold text-white rounded-lg bg-green-400" to="/sign-up">Sign Up</router-link>
   </div>
 </template>
 
